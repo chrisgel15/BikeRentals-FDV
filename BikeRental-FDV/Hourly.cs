5681acd78444190068478ce8aaf0b273a85baa40
Christian Gelman <christiangelman@gmail.com>
2017-12-07 01:54:44 -0300

    Testing push

--- BikeRental-FDV/Hourly.cs
+++ BikeRental-FDV/Hourly.cs
@@ -5,7 +5,7 @@ namespace BikeRental_FDV
     public class Hourly : Subscription
     {
         // Using class variables as it is the same for all the instances. Keep it simple
-        public static int charge = 5;
+        public static int charge = 5; 
 
         public override double CalculateRentalCost(DateTime initialTime, DateTime endTime)
         {
